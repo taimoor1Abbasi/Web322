bbcb534223ba94bae5b9ece5dcf1ed241891c5cd
taimoor1Abbasi <110571850+taimoor1Abbasi@users.noreply.github.com>
2023-01-16 16:21:07 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+    res.send("Taimoor Abbasi 156265217");
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT, onHttpStart);
